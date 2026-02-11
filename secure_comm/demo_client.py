f352256f7a41178c0e2868e79feb0c00223946e0
maorishakov <maorishakov@gmail.com>
2026-02-11 20:43:51 +0200

    Add persistent TCP client and server demo

--- secure_comm/demo_client.py
+++ secure_comm/demo_client.py
@@ -1,6 +1,5 @@
 from secure_comm.transport.tcp_client import TCPClient
 from secure_comm.protocol.message import Message, MessageType
-from enum import IntEnum
 
 
 def main():
@@ -8,9 +7,12 @@ def main():
     port = 9000
 
     tcp_client = TCPClient(host, port)
-    msg = Message(msg_type=MessageType.DATA, seq=1, payload=b"hello secure world")
+    tcp_client.connect()
+    for i in range(1, 4):
+        msg = Message(msg_type=MessageType.DATA, seq=i, payload=b"hello secure world")
+        tcp_client.send(msg)
 
-    tcp_client.send_message(msg)
+    tcp_client.close()
 
 
 if __name__ == "__main__":
